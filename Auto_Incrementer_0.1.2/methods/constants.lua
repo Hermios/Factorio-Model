75d0b72bdc4bf17dc738a3ac32c8eb57dd4482f6
Hermios <nicoblex@yahoo.fr>
2018-11-23 00:40:33 +0100

    filter created combinators on name, when built.

---
+++ Auto_Incrementer_0.1.2/methods/constants.lua
@@ -0,0 +1,4 @@
+ModName="Auto_Incrementer"
+AutoIncrementCombinator="Auto Increment Combinator"
+AutoIncrementCombinatorRecipe="Auto Increment Combinator Recipe"
+AicSignal="signal-Aic"
---
+++ Auto_Incrementer_0.1.2/prototypes/recipes.lua
@@ -0,0 +1,13 @@
+data:extend({
+    {
+        type = "recipe",
+        name = AutoIncrementCombinatorRecipe,
+        enabled = false,
+        ingredients = {
+			{"copper-cable", 4},
+			{"electronic-circuit", 1}
+		},
+        result = AutoIncrementCombinator,
+        result_count = 1,
+    },
+})
