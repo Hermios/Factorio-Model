5b7d43b48dc2d021723b69c9e58601a8d9c4fb30
Hermios <nicoblex@yahoo.fr>
2019-03-14 23:57:28 +0100

    version 0.17 new libs

---
+++ TrainsWhistle_0.3.1/migrations/updateGlobalData.lua
@@ -0,0 +1,7 @@
+for key,value in pairs(global.listTrains) do
+	global.listTrains[key].guiData=global.listTrains[key].guiData or value
+end
+
+for key,value in pairs(global.listTrainStops) do
+	global.listTrainStops[key].guiData=global.listTrainStops[key].guiData or value
+end
---
+++ TrainsWhistle_0.3.1/prototypes/recipes.lua
@@ -0,0 +1,10 @@
+createData("recipe","exoskeleton-equipment",trainWhistleEquipment,
+{
+	enabled = false,
+	ingredients = 
+	{
+		{"copper-cable", 5},
+		{"advanced-circuit", 2},
+		{"iron-plate", 5},
+	}
+})
