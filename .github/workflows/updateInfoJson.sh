780a646500e996da9d14ac386a2e1d1d487c9a55
Hermios <nicoblex@yahoo.fr>
2023-12-29 13:10:15 +0200

    Update updateInfoJson.sh

--- .github/workflows/updateInfoJson.sh
+++ .github/workflows/updateInfoJson.sh
@@ -1,8 +1,13 @@
 jsonFile="$1/info.json"
 jsonString=`cat $jsonFile`
 currentVersion=$(jq -r '.version' <<< "$jsonString")
+versionLevel=expr $2 + 0
 IFS='.' read -ra VersionData <<< "$currentVersion"
-VersionData[$2]=expr ${VersionData[$2]}+1
+VersionData[$versionLevel]=expr ${VersionData[$versionLevel]}+1
+for i in (versionLevel+1..2)
+do
+  VersionData[$versionLevel]=0
+done
 newVersion=${VersionData[0]}.${VersionData[1]}.${VersionData[2]}
 
 jq -c '.version = "$newVersion"' $jsonFile > tmp.$$.json && mv tmp.$$.json $jsonFile
