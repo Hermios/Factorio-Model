a68e53678cec7a79f7b023a045906ab09359a6ae
Hermios <nicoblex@yahoo.fr>
2023-12-31 16:19:07 +0200

    Update DuplicateFactorioModel.sh

--- .scripts/DuplicateFactorioModel.sh
+++ .scripts/DuplicateFactorioModel.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-read modName
-read description
+read -p "Name of the mod? " modName
+read -p "Description? "description
 gh repo create $modName -d "Factorio Mod: $description" --template https://github.com/Hermios/Factorio-Model.git --include-all-branches --public
+
+#Delete all existing labels
+labels=$(gh label list -R Hermios/$modName --json name)
+jq -c '.[]' <<< $labels | while read i
+do 
+  label=$(jq '.name '<<< $i)
+  gh label delete $label -R Hermios/$modName
+done
+gh label clone Hermios/Factorio-Model -R Hermios/$modName
