b297c4dca4969fc1981201a872e31f04bbbc0acd
Hermios <nicoblex@yahoo.fr>
2023-12-31 16:40:22 +0200

    Update DuplicateFactorioModel.sh

--- .scripts/DuplicateFactorioModel.sh
+++ .scripts/DuplicateFactorioModel.sh
@@ -8,6 +8,7 @@ labels=$(gh label list -R Hermios/$modName --json name)
 jq -c '.[]' <<< $labels | while read i
 do 
   label=$(jq '.name '<<< $i | tr -d '"')
-  gh label delete --yes "$label" -R Hermios/$modName
+  echo "$label"
+  gh label delete "$label" --yes -R Hermios/$modName
 done
 gh label clone Hermios/Factorio-Model -R Hermios/$modName
