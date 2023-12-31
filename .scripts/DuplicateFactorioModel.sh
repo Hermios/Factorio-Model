000883c9761e55206417adc84968ba359c6cae1d
Hermios <nicoblex@yahoo.fr>
2023-12-31 15:25:26 +0200

    Update DuplicateFactorioModel.sh

--- .scripts/DuplicateFactorioModel.sh
+++ .scripts/DuplicateFactorioModel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read newMod
-git clone --bare https://github.com/EXAMPLE-USER/OLD-REPOSITORY.git
-cd OLD-REPOSITORY.git
-git push --mirror https://github.com/EXAMPLE-USER/NEW-REPOSITORY.git
+git clone --bare https://github.com/Hermios/Factorio-Model.git
+cd Factorio-Model.git
+git push --mirror https://github.com/EXAMPLE-USER/$newMod.git
 cd ..
-rm -rf OLD-REPOSITORY.git
+rm -rf Factorio-Model.git
