13fa69ef77b77db96c2fcb2602c9ac2e33e3421b
Hermios <nicoblex@yahoo.fr>
2023-12-31 15:35:25 +0200

    Update DuplicateFactorioModel.sh

--- .scripts/DuplicateFactorioModel.sh
+++ .scripts/DuplicateFactorioModel.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 read modName
 read description
-gh repo create $modName -d "Factorio Mod: $description"
-git clone --bare https://github.com/Hermios/Factorio-Model.git
-cd Factorio-Model.git
-git push --mirror https://github.com/EXAMPLE-USER/$modName.git
-cd ..
-rm -rf Factorio-Model.git
+gh repo create $modName -d "Factorio Mod: $description" --template https://github.com/Hermios/Factorio-Model.git --include-all-branches --public
