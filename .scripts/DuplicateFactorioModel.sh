4d830f7df03484f38f5c83bce72ea8a858e8034c
nicolas.blexmann <nicolas.blexmann@ext.verallia.com>
2024-01-01 19:25:12 +0200

    remove owner for new created rep

--- .scripts/DuplicateFactorioModel.sh
+++ .scripts/DuplicateFactorioModel.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 #Get inputs
-read -p "Name of the mod? " modName
 read -p "Title of the mod? " titleName
 read -p "Description? " description
 
+modName="${titleName// /_}"
+
 #Create repo
-gh repo create $modName -d "Factorio Mod: $description" --template https://github.com/Hermios/Factorio-Model.git --include-all-branches --public
+newRep=gh repo create $modName -d "Factorio Mod: $description" --template https://github.com/Hermios/Factorio-Model.git --include-all-branches --public
+
 
 #Delete all existing labels
-labels=$(gh label list -R Hermios/$modName --json name)
+labels=$(gh label list -R $newRep --json name)
 jq -c '.[]' <<< $labels | while read i
 do 
   label=$(jq '.name '<<< $i | tr -d '"')
-  gh label delete "$label" --yes -R Hermios/$modName
+  gh label delete "$label" --yes -R $newRep
 done
 
 #Clone labels from model
-gh label clone Hermios/Factorio-Model -R Hermios/$modName
+gh label clone Hermios/Factorio-Model -R $newRep
 
 #Clone secrets
-gh secret set -f AppData/Roaming/GitHub\ CLI/.env -R Hermios/Test
+gh secret set -f AppData/Roaming/GitHub\ CLI/.env -R $newRep
